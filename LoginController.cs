1f6ae85f1d0fd5e0606319a087dbfe9154507596
Ritik269 <96427152+Ritik269@users.noreply.github.com>
2023-07-21 23:43:30 +0530

    Add files via upload

---
+++ LoginController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Findpropertymain.Models;
+
+namespace Findpropertymain.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly propertyslnContext _context;
+
+        public LoginController(propertyslnContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Helps to view the log in controller
+        public async Task<IActionResult> Index()
+        {
+            var propertyslnContext = _context.Users.Include(u => u.Type);
+            return View(await propertyslnContext.ToListAsync());
+        }
+
+        // GET: Helps to view the login controller
+        public async Task<IActionResult> Details(short? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .Include(u => u.Type)
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: Login/Create
+        public IActionResult Create()
+        {
+            ViewData["TypeId"] = new SelectList(_context.UserTypes, "TypeId", "TypeName");
+            return View();
+        }
+
+        // POST: Login/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserId,UserName,UserMobileNumber,UserEmailId,UserPassword,TypeId")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TypeId"] = new SelectList(_context.UserTypes, "TypeId", "TypeName", user.TypeId);
+            return View(user);
+        }
+
+        // GET: Login/Edit/5
+        public async Task<IActionResult> Edit(short? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            ViewData["TypeId"] = new SelectList(_context.UserTypes, "TypeId", "TypeName", user.TypeId);
+            return View(user);
+        }
+
+        // POST: Login/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(short id, [Bind("UserId,UserName,UserMobileNumber,UserEmailId,UserPassword,TypeId")] User user)
+        {
+            if (id != user.UserId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.UserId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TypeId"] = new SelectList(_context.UserTypes, "TypeId", "TypeName", user.TypeId);
+            return View(user);
+        }
+
+        // GET: Login/Delete/5
+        public async Task<IActionResult> Delete(short? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .Include(u => u.Type)
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: Login/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(short id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(short id)
+        {
+            return _context.Users.Any(e => e.UserId == id);
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace Findpropertymain.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            InterestedCustomers = new HashSet<InterestedCustomer>();
+            Properties = new HashSet<Property>();
+        }
+        [Required(ErrorMessage = "Enter User Id ")]
+        //[RegularExpression("[^0-9]", ErrorMessage = "User Id must be numeric")]  // validations
+        public short UserId { get; set; }
+        [Required(ErrorMessage = "Enter User Name")]
+        [StringLength(60, MinimumLength = 4)]
+        //[RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]  // validations
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Enter Mobile Number ")]
+        //[RegularExpression("[^0-9]", ErrorMessage = "Mobile Number must be numeric")]  // validations
+
+        public string UserMobileNumber { get; set; }
+
+        [Required(ErrorMessage = "Enter your Email")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "E-mail is not valid")]
+
+        public string UserEmailId { get; set; }
+        [Required(ErrorMessage = "Enter Password")]
+        [StringLength(60, MinimumLength = 4)]
+        //[RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]  // validations
+        public string UserPassword { get; set; }
+
+        public short? TypeId { get; set; }
+
+        public virtual UserType Type { get; set; }
+        public virtual ICollection<InterestedCustomer> InterestedCustomers { get; set; }
+        public virtual ICollection<Property> Properties { get; set; }
+    }
+}
